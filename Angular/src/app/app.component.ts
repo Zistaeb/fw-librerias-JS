ec1c61b8f49ca5cb6b7eab5cee62bb3191acf197
Betty <bperezatencio@gmail.com>
2023-08-28 16:50:49 -0300

    Componentes en Angular con CSS

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -10,7 +10,8 @@ type Movies = Array<Movie>;
 @Component({
   selector: 'app-root',
   template: `<h2>Peliculas</h2>`,
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: [`./app.component.css`]
 })
 
 export class AppComponent {
